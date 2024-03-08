b5c52a331f768a538b8b7c44bcbf7953bb1c2611
MichaelWK04 <michaelwalker1358@gmail.com>
2024-03-08 02:00:30 -0500

    Completed Flask implementation

--- Question1/Question1.py
+++ Question1/Question1.py
@@ -3,8 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def default():
+    return "<h1>Welcome to my CSCB20 website!</h1>"
+
+@app.route('/<name>')
+def greet(name):
+    return user(name)
+
+def user(name):
+    new_name = ""
+    if name.isupper() and name.isalpha():
+        new_name = name.lower()
+    elif name.islower() and name.isalpha():
+        new_name = name.upper()
+    else:
+        is_first = False
+        for letter in name:
+            if (letter.isalpha()):
+                if (not is_first):
+                    new_name = letter.upper()
+                    is_first = True
+                else:
+                    new_name += letter.lower()
+    return f"<h1>Welcome, {new_name}, to my CSCB20 website</h1>"
 
 if __name__ == '__main__':
     app.run()
