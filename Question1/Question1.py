1deafb212d25e5095d3516e5b56943732abae69c
MichaelWK04 <michaelwalker1358@gmail.com>
2024-03-08 22:45:27 -0500

    changed way html tag is stored in user method

--- Question1/Question1.py
+++ Question1/Question1.py
@@ -11,21 +11,23 @@ def greet(name):
     return user(name)
 
 def user(name):
-    new_name = ""
+    new_name = "<h1>Welcome, "
     if name.isupper() and name.isalpha():
-        new_name = name.lower()
+        new_name += name.lower()
     elif name.islower() and name.isalpha():
-        new_name = name.upper()
+        new_name += name.upper()
     else:
         is_first = False
         for letter in name:
             if (letter.isalpha()):
                 if (not is_first):
-                    new_name = letter.upper()
+                    new_name += letter.upper()
                     is_first = True
                 else:
                     new_name += letter.lower()
-    return f"<h1>Welcome, {new_name}, to my CSCB20 website</h1>"
+    new_name += ", to my CSCB20 website</h1>"
+    print(new_name)
+    return new_name
 
 if __name__ == '__main__':
     app.run()
